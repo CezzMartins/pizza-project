0a62d3af2da3d7cf2a8724e82cae9b2342d47b3f
CezzMartins <cezzmartins@gmail.com>
2021-10-31 01:20:53 -0300

    feat: add data modal

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-const selectItem = (el) => {
-    return document.querySelector(el)
-}
-const selectAll = (el) => {
-    return document.querySelectorAll(el);
-}
+const selectItem = (el) => { return document.querySelector(el); }
+const selectAll = (el) => {return document.querySelectorAll(el);}
+let modalQty = 1;
 
 
 pizzaJson.map((pizza, index) => {
@@ -21,18 +18,26 @@ pizzaJson.map((pizza, index) => {
     // PopUP show up 
     pizzaSingle.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault();
+        modalQty = 1;
+
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
         
         selectItem('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
         selectItem('.pizzaBig img').src = pizzaJson[key].img;
         selectItem('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
 
-        selectAll('.pizzaInfo--size').forEach((size, sizeIndex) => {
+        selectItem('.pizzaInfo--size.selected').classList.remove('selected')
 
+        selectAll('.pizzaInfo--size').forEach((size, sizeIndex) => {
+            if(sizeIndex == 2){
+                size.classList.add('selected')
+            }
 
             size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
         })
 
+        // quantity default of modal
+        selectItem('.pizzaInfo--qt').innerHTML = modalQty
 
         selectItem('.pizzaInfo--actualPrice').innerHTML = `${pizzaJson[key].price.toFixed(2)}`
 
